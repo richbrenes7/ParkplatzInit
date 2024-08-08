40207fee7b65fd8c3b39fcae8b44808b771d14cc
RichBrenes7 <nbrenesl@miumg.edu.gt>
2024-08-08 09:37:42 -0600

    Cambios

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,4 @@ import './data.js';
 import './view.js';
 import './controller.js';
 
-console.log('App initialized');
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Hello world listening on port', port);
-});
+console.log('App Inicializada');
