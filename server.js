b972400b61d7ec26a6f275dfcdc6a30cf582a338
RichBrenes7 <nbrenesl@miumg.edu.gt>
2024-08-08 09:27:41 -0600

    Cambios

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Sirve los archivos estáticos desde el directorio 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+// Servir los archivos estáticos desde el directorio 'dist'
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Maneja todas las rutas y retorna el archivo index.html
+// Ejemplo de uso de Axios
+app.get('/api/data', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.example.com/data');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Manejar todas las rutas y devolver el archivo index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(PORT, () => {
